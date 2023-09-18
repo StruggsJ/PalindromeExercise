12fb77141d0f11b362fb556606c8869a138edb2b
StruggsJ <strjac5@gmail.com>
2023-09-18 11:51:24 -0500

    Initial commit.

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string input)
+        {
+            var toLower = input.ToLower(); 
+
+            char[] inputChars = toLower.ToCharArray();
+            Array.Reverse(inputChars);
+
+            string checkPalindrome = new String(inputChars);
+
+            if (checkPalindrome == toLower)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+﻿using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,28 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+
+        [InlineData("Radar", true)] // ← you will need to put some test data and expected result here.  For example [InlineData(“racecar”, true)]
+        [InlineData("Plate", false)]
+        [InlineData("Noon", true)]
+        [InlineData("Card", false)]
+        [InlineData("Dad", true)]
+
+        
+        public void IsAPalindromeTest (string input, bool expected)
         {
+
+            //Arrange
+
+            var instance = new WordSmith();
+
+            //Act
+
+            var actual = instance.IsAPalindrome(input);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
